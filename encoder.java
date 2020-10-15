2a064556bdb1da7805d27ededabd008b88dd8696
KatherineH7 <72330186+KatherineH7@users.noreply.github.com>
2020-10-15 14:27:35 -0400

    Create encoder.java

---
+++ encoder.java
@@ -0,0 +1,16 @@
+import java.util.*
+
+public class encoder
+{
+  char[] alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  char key;
+  String plaintext;
+  
+  public encode()
+  {
+    int a = 0;
+    int b = 0;
+    for (int c = 0; 
+    int a = ArrayUtils.indexOf(alphabet, key);
+    
+  

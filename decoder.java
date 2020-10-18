a091ccb2975ab2351ff43aa0795c81ccab34b666
KatherineH7 <khlaing10@stuy.edu>
2020-10-18 18:30:14 -0400

    merged encode and decode into same file, made it to accept parameters

--- decoder.java
+++ decoder.java
@@ -1,24 +1,94 @@
 import java.util.*;
 import java.lang.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class encoder
+public class decoder
 {
   public static void main(String args[])
   {
-  char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-  String key = "BVNR";
-  String plaintext = "DVYCNZVJIHNVMNBDFTRRSNNXPIRMFMZZOYUFXGBEHKEVDDFVMTURWDAXMDGKMZBIOJZFOZLZOHLGVMFVBIQEPOUZOBCRSOVTVGNIUJVEUZEVTOZVPIFYPMRZUCBLHCGZXJHCENNZMVOFVONCJOGCFVAUTZRKIZJRUZEPQVEKPAGYFRBIMYVKJNNNBTVYBQRFGYEZWDAXPASKIZFGMZREBIQIFBHCBOVEHOUVDDETVGNKJJANIZAVWZEZGDAUNTFVMATIPRVEHBEZNVOFVOGYFHBLUCJYFIRMFMVKJNNUBHCUSDMQMTAFWZZSFMVENTFFVGJYFIRMFMVWJIQDZNRCGDAMPGHEUVEZMTCRVNVEHWRWPMRTPASZORNIFCBLTZFROYOIJITZOBHGUCRIFVEFGZIVSTSLOZERMDZVFONEEZFGFXVRMGLNIZAVWZEDZCLGPNTVUNHTIVALQKRIIVAUPAZVUCNKJOEVRPVIFNNJUMBEHHBIBGCIJIPZQGRKPKEVWZAKNZSIPHQVMDOVSVGVMTFKFKCZOBVEUJGYFNGIFZGROYZVUCBUJXNCMTXEPXXZOBCVPKYVTCNKTJSWUCREJVPTPPAKJOUZHCGZNZGFHZGKPNRRBNFFPINJJXNEUCVJJNZPTPOJUDGLUZSFSKVJUJYROYORMGJZUCNGIDYFTJCYJXNCGGBLSDFYDVGFUCEFXNUZNNRCGPCFOCVJTRBIEDDLJZGCZONBFOBKIZFYJKGYFMRZTIBKIDAXTPEGSDFZOBVEUCVJJAGYFTOLUFAVXDGRMHBJUVYCNZAZOOUVJMQVHMRVTJZVUDZVPMBKIZETIZEZTCIVSTAVBMYPUCRJBHRWFZYZOBFKPRNIENGYFJPVBIJZUCZVUCRIFIBNJNLFVMVETPYRSXVKZJSKIZZROCNKUJRJCZYKFYEFVIQSZRURSQRJBNVEEDNEJNYVTWLTPMNCSZRWTXBDNZETFNHISJHEENVKXDGYIZEJVMSIJBUKBIQCFAGKIZFKSZRKTONBFTBLXVGVSRNIEDGJFSGIFHRUPRAKPRAZTOUVCVGKFMLNIZEVUCNKOJOCFHBCFDFNBNUVEWLNBQRJBIQTPJYVEWLSSZRQFNJYJXURGZJYPPEJQMRMJJHJXZEVPPGFGNVXIOBWMVAUMJBBBOGYFXEFXYFFGRNKFMTRAZEJUCRIF";
+    File plaintext = null;
+    File keytext = null;
+    String method = "";
+    String message = "";
+    String key = "";
 
-  int a = 0;
-  int b = 0;
+    if (0 < args.length)
+    {
+        method = new String(args[0]);
+        plaintext = new File(args[1]);
+        keytext = new File(args[2]);
+    }
+    else
+    {
+    System.err.println("Invalid arguments count:" + args.length);
+    System.exit(0);
+    }
+
+      BufferedReader br = null;
+
+      try
+      {
+        String sCurrentLine;
+
+        br = new BufferedReader(new FileReader(plaintext));
+
+        while ((sCurrentLine = br.readLine()) != null)
+          {
+            message += sCurrentLine;
+          }
+
+          BufferedReader br2 = null;
+
+         br2 = new BufferedReader(new FileReader(keytext));
+
+         while ((sCurrentLine = br2.readLine()) != null)
+           {
+             key += sCurrentLine;
+           }
+
+        }
+
+       catch (IOException e)
+        {
+          e.printStackTrace();
+        }
+
+        finally
+        {
+          try
+          {
+            if (br != null)br.close();
+          }
+          catch (IOException ex)
+          {
+            ex.printStackTrace();
+          }
+        }
+
+        boolean shift = true;
+        if (method.equals("decode"))
+          shift = false;
+
+        System.out.print(Vigenere(message, key, shift));
+
+      }
 
 
 
-  for (int c = 0; c < plaintext.length(); c++)
+  static String Vigenere(String text, String key, Boolean encode)
+  {
+    char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    int a = 0;
+    int b = 0;
+    String result = "";
+    for (int c = 0; c < text.length(); c++)
     {
       for (int i = 0; (i < alphabet.length); i++)
       {
-        if (alphabet[i] == plaintext.charAt(c))
+        if (alphabet[i] == text.charAt(c))
             a = i;
       }
       for (int i = 0; (i < alphabet.length); i++)
@@ -26,8 +96,11 @@ public class encoder
         if (alphabet[i] == key.charAt(c % 4))
             b = i;
       }
-      System.out.print(alphabet[(Math.abs(a - b)) % 26]);
+      if (encode == false)
+        result += alphabet[(Math.abs(a - b)) % 26];
+      else
+        result += alphabet[((a + b)) % 26];
     }
+    return result;
   }
-
-  }
+}
